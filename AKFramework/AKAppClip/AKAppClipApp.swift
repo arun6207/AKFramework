706d2ff13764f01eb11f40b94553768adfbd8fd3
Arun Amuri <arun.amuri@dnata.com>
2021-11-10 11:46:22 +0400

    new changes

---
+++ AKFramework/AKAppClip/AKAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AKAppClipApp.swift
+//  AKAppClip
+//
+//  Created by Arun Amuri on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AKAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
