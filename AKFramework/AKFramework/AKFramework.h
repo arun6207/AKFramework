4282f28401d7a0b70222768bcae79780e4cac192
Arun Amuri <arun.amuri@dnata.com>
2021-11-10 11:44:14 +0400

    initial commit

---
+++ AKFramework/AKFramework/AKFramework.h
@@ -0,0 +1,18 @@
+//
+//  AKFramework.h
+//  AKFramework
+//
+//  Created by Arun Amuri on 10/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKFramework.
+FOUNDATION_EXPORT double AKFrameworkVersionNumber;
+
+//! Project version string for AKFramework.
+FOUNDATION_EXPORT const unsigned char AKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKFramework/PublicHeader.h>
+
+
